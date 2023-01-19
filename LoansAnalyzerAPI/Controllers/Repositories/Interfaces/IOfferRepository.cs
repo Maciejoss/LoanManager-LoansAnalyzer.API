0e6ba5e999b05616be430e2f5a21fe2fb60e00d2
Jakub Borek <01158592@pw.edu.pl>
2023-01-19 19:23:02 +0100

    Create basic document handling

--- LoansAnalyzerAPI/Controllers/Repositories/Interfaces/IOfferRepository.cs
+++ LoansAnalyzerAPI/Controllers/Repositories/Interfaces/IOfferRepository.cs
@@ -5,8 +5,10 @@ namespace LoansAnalyzerAPI.Controllers.Repositories.Interfaces
     public interface IOfferRepository
     {
         Task<IEnumerable<OfferDto>?> GetAllOffersAsync();
-        Task<string> GetDocument(int id);
+        Task<OfferDto> GetOfferAsync(int id);
+        Task<string> GetDocumentAsync(int id);
         Task<bool> ChangeOfferState(ChangeOfferStateDTO changeOfferStateDTO);
+        Task SaveDocument(int offerId, DocumentDto document);
 
     }
 }
